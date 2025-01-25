b27260bf8ecdcf9754c781b77799818a638d0bca
moppiiiii <tnktmhssrc@gmail.com>
2025-01-25 16:02:31 +0900

    build: vite の build を lib mode  に変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "react-gantt-flow",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
