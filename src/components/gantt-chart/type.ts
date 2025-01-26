fcfbcd6dfba7f77e85304702e7d4623675c262ec
moppiiiii <tnktmhssrc@gmail.com>
2025-01-26 11:31:35 +0900

    feature: task の型定義を実装

---
+++ src/components/gantt-chart/type.ts
@@ -0,0 +1,11 @@
+import type { Task } from "../../types/task";
+
+/**
+ * @description GanttChart props
+ */
+export type GanttChartProps = {
+  /**
+   * @description Tasks
+   */
+  task: Task[];
+};
