22fa48d3b55d62d3ae07e2c6fa144e67dc6c3cce
moppiiiii <tnktmhssrc@gmail.com>
2025-03-16 16:33:55 +0900

    feature: today-line-component

---
+++ src/components/gantt-chart/_parts/today-line/type.ts
@@ -0,0 +1,14 @@
+export type TodayLineProps = {
+  /**
+   * @description Date to x
+   */
+  dateToX: (date: Date) => number;
+  /**
+   * @description Axis height
+   */
+  axisHeight: number;
+  /**
+   * @description Chart height
+   */
+  chartHeight: number;
+};
