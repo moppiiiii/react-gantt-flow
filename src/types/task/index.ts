98e8b3926bfc950a2c543ff263d0428c3219b1a5
moppiiiii <tnktmhssrc@gmail.com>
2025-06-14 18:47:54 +0900

    refactor: show dispality conditions

--- src/types/task/index.ts
+++ src/types/task/index.ts
@@ -29,8 +29,4 @@ export type Task = {
    * The start date of the task used for scheduling.
    */
   startDate: Date;
-  /**
-   * The status of the task.
-   */
-  status: "todo" | "inProgress" | "done";
 };
