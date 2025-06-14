9f7ad3a22437150ef01ba0d93ebf9bcc3f88b1bb
moppiiiii <tnktmhssrc@gmail.com>
2025-06-14 15:29:14 +0900

    refactor: export gantt flow type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as GanttFlow } from "./components/GanttFlow";
 export * from "./components/GanttFlow";
+export * from "./components/GanttFlow/type";
 
 export * from "./types/task";
